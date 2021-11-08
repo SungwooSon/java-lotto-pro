34591567292bdf8af97b09b708e06e89da25e9b4
sungwoo <ssw890418@gmail.com>
2021-11-08 20:03:08 +0900

    refactor:피드백 반영

---
+++ src/main/java/study/Numbers.java
@@ -0,0 +1,31 @@
+package study;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Numbers {
+
+    private static final List<Number> numbers = new ArrayList<>();
+    private static int result;
+
+    public static void makeNumberGroup(Text text) {
+        String[] strings = text.splitWithDelimeter();
+        for(String string : strings) {
+            numbers.add(new Number(string));
+        }
+    }
+
+    public static int sumAllNumbers() {
+        Iterator<Number> iterator = numbers.iterator();
+        while(iterator.hasNext()) {
+             result += iterator.next().getNumber();
+        }
+        return result;
+    }
+
+    public static void cleanUp() {
+        numbers.clear();
+        result = 0;
+    }
+}

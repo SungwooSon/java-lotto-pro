3278e765955c78f919050f0769be84e3c8cb9555
sungwoo <ssw890418@gmail.com>
2021-11-11 02:13:12 +0900

    style:패키지 구조 수정

---
+++ src/main/java/study/lotto/Main.java
@@ -0,0 +1,8 @@
+package study.lotto;
+
+public class Main {
+    public static void main(String[] args) {
+        LottoController controller = new LottoController();
+        controller.run();
+    }
+}
